619255e49183d119ee142f9b06857bb3ad3c272e
funnker <67109377+funnker@users.noreply.github.com>
2020-07-06 00:09:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -123,6 +123,34 @@ client.on('messageReactionRemove', async (reaction, user) => {
     }  
 }) */
 
+        //LOGS STUFF
+
+        client.on('messageDelete', async message => {
+          // ignore direct messages
+          if (!message.guild) return;
+          const fetchedLogs = await message.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MESSAGE_DELETE',
+          });
+          // Since we only have 1 audit log entry in this collection, we can simply grab the first one
+          const deletionLog = fetchedLogs.entries.first();
+        
+          // Let's perform a sanity check here and make sure we got *something*
+          if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
+        
+          // We now grab the user object of the person who deleted the message
+          // Let us also grab the target of this action to double check things
+          const { executor, target } = deletionLog;
+        
+        
+          // And now we can update our output with a bit more information
+          // We will also run a check to make sure the log we got was for the same author's message
+          if (target.id === message.author.id) {
+            client.channels.get(`729442853238866010`).send(`A message by ${message.author.tag} was deleted by ${executor.tag}.`);
+          }	else {
+            client.channels.get(`729442853238866010`).send(`A message by ${message.author.tag} was deleted, but we don't know by who.`);
+          }
+        });
 
 client.login(token);
 

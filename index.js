2a259e9b4c5a5cf97c86ef17f82d0a90e1afb690
funnker <67109377+funnker@users.noreply.github.com>
2020-07-17 21:50:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,17 +18,6 @@ for(const file of commandFiles)
 var ondutyStaff = 0;
 let DutyRole = '724607373380943936';
 
-const HelperEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Helper Application')
-	.setDescription('Apply here: shorturl.at/qrzC3')
-  .addField('Status: OPEN', true)
-  
-const PREmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Helper Application')
-	.setDescription('Apply here: shorturl.at/rsHS2')
-	.addField('Status: OPEN', true)
 
 client.on('ready', () => {
     console.log('ONLINE')
@@ -41,6 +30,19 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
+
+  const HelperEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Helper Application')
+	.setDescription('Apply here: shorturl.at/qrzC3')
+  .addField('Status: OPEN', true)
+  
+const PREmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Helper Application')
+	.setDescription('Apply here: shorturl.at/rsHS2')
+  .addField('Status: OPEN', true)
+  
   if(!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);

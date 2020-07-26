ab14b2f24f4a939427067afe4825bf3522a30c7b
funnker <67109377+funnker@users.noreply.github.com>
2020-07-26 13:22:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ for(const file of commandFiles)
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
-
+//ok boss
 var ondutyStaff = 0;
 let DutyRole = '724607373380943936';
 
